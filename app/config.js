e10050d5d2216a8d6b657487d599320aeddd7cb1
Aaike Van Roekeghem <aaikevr@gmail.com>
2015-06-04 21:31:58 +0700

    Update 2015-06-04T14:31:49.200Z

---
+++ app/config.js
@@ -0,0 +1,1 @@
+System.register([],function(e){"use strict";var l;return{setters:[],execute:function(){l={transitions:[{value:"faded",label:"faded"},{value:"faded-half",label:"faded-half"},{value:"left",label:"left"},{value:"right",label:"right"},{value:"top",label:"top"},{value:"bottom",label:"bottom"}]},e("config",l)}}});
